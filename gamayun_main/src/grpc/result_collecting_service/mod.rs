66c889cd87ada313f357d6168618ab8aada8db94
Ivan Brko <ivan.brko@protonmail.com>
2024-10-04 18:10:19 +0200

    Removing reporting functions not needed anymore

--- gamayun_main/src/grpc/result_collecting_service/mod.rs
+++ gamayun_main/src/grpc/result_collecting_service/mod.rs
@@ -1,15 +1,12 @@
 mod common_utils;
 mod impl_result_maps_only;
 
-use crate::config::job_config::JobConfig;
 use crate::init::AppContext;
 use protos::gamayun::result_server::Result;
-use protos::gamayun::{
-    EmptyResponse, JobError, JobResultWithMapAndStrings, JobResultWithMapOnly,
-    JobResultWithRawStringsOnly,
-};
+use protos::gamayun::{EmptyResponse, JobError, JobResultWithMapOnly};
 use tonic::{Request, Response, Status};
-use tracing::{error, info};
+use tracing::{error, info, instrument, Span};
+use tracing_futures::Instrument;
 
 pub struct ResultCollectingService {
     app_context: AppContext,
@@ -23,34 +20,27 @@ impl ResultCollectingService {
 
 #[tonic::async_trait]
 impl Result for ResultCollectingService {
-    async fn report_result_with_raw_strings_only(
-        &self,
-        request: Request<JobResultWithRawStringsOnly>,
-    ) -> std::result::Result<Response<EmptyResponse>, Status> {
-        let job_result = request.into_inner();
-        info!("Received raw strings only result: {:?}", job_result);
-        Ok(Response::new(EmptyResponse {}))
-    }
-
     async fn report_result_with_map_only(
         &self,
         request: Request<JobResultWithMapOnly>,
     ) -> std::result::Result<Response<EmptyResponse>, Status> {
         let job_result = request.into_inner();
-        info!("Received map only result: {:?}", job_result);
 
-        self.handle_result_map_only(job_result).await
+        // Create a span with `name` and `runId` added to the tracing context
+        let span = tracing::info_span!(
+            "report_result_with_map_only",
+            name = %job_result.name,
+            run_id = %job_result.run_id
+        );
+
+        // Log that we received the result with the span context
+        info!(parent: &span, "Received map only result: {:?}", job_result);
+
+        // Instrument the future to use the span for subsequent logs
+        self.handle_result_map_only(job_result)
+            .instrument(span)
+            .await
     }
-
-    async fn report_result_with_map_and_strings(
-        &self,
-        request: Request<JobResultWithMapAndStrings>,
-    ) -> std::result::Result<Response<EmptyResponse>, Status> {
-        let job_result = request.into_inner();
-        info!("Received map and strings result: {:?}", job_result);
-        Ok(Response::new(EmptyResponse {}))
-    }
-
     async fn report_error(
         &self,
         request: Request<JobError>,
